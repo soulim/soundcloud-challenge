c7fe87e46144f111295f057e411801e540ae2aeb
Alex Soulim <soulim@gmail.com>
2012-06-11 13:43:42 +0400

    Add Rack::UploadProgress options

--- lib/rack/upload_progress.rb
+++ lib/rack/upload_progress.rb
@@ -1,8 +1,16 @@
 # Rack middleware to handle upload process with progress indication
 module Rack
   class UploadProgress
-    def initialize(app)
-      @app = app
+    UPLOAD_PATH     = '/uploads'
+    STATUS_PATH     = '/status'
+    QUERY_PARAMETER = 'X-Progress-ID'
+    
+    def initialize(app, options = {})
+      @app     = app
+      @options = {
+          upload_path: UPLOAD_PATH,
+          status_path: STATUS_PATH
+        }.merge!(options)
     end
 
     def call(env)
